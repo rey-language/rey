8450a901e70a031efde452e8036b31c5f70ae19b
Misbah Khursheed <m.misbahkhursheed@gmail.com>
2025-12-23 22:55:42 +0530

    added v0 lexer token

--- spec/v0/src/lexer/lexer.rs
+++ spec/v0/src/lexer/lexer.rs
@@ -1,5 +1,54 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Token {
     pub kind: TokenKind,
     pub span: Span,
+}
+
+//this enum will grow over time
+// for v0, keeping it simple
+//contains all tokens supported for v0
+pub enum TokenKind {
+    //single char tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    Percent,
+
+    //keywords
+    Var,
+    Func,
+    Return,
+    If,
+    Else,
+    While,
+    For,
+    True,
+    False,
+    Null,
+
+    //literals
+    Identifier(String),
+    StringLiteral(String),
+    NumberLiteral(f64),
+
+    //operators
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    And,
+    Or,
+
+    //special
+    Eof,
 }
